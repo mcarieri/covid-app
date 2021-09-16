e4ec8a993ac47aa6fca8272b74fed19176224395
Bomb Atomically <ieugenelee@gmail.com>
2021-09-15 18:41:56 -0700

    edited handlebar templates - colors and text

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -3,4 +3,20 @@ router.get('/', function(req, res) {
     res.render('index');
 });
 
+router.get('/index', function(req, res) {
+    res.render('index');
+});
+
+router.get('/search', function(req, res) {
+    res.render('search');
+});
+
+router.get('/signup', function(req, res) {
+    res.render('signup');
+});
+
+router.get('/login', function(req, res) {
+    res.render('login');
+});
+
 module.exports = router;
