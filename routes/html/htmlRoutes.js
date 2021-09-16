7a572df458d96204559e39a49038da6221b0c1b0
Bomb Atomically <ieugenelee@gmail.com>
2021-09-15 17:56:44 -0700

    added html routes and setup server.js

---
+++ routes/html/htmlRoutes.js
@@ -0,0 +1,6 @@
+let router = require('express').Router();
+router.get('/', function(req, res) {
+    res.render(index);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,42 @@
+const path = require('path');
 const express = require('express');
+// const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// commented out basic database connection and authentication 
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// for authentication **
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const html_routes = require('./routes/html_routes');
+app.use(html_routes);
+
+// app.use(require('./controllers/'));
+
+// sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+// });
