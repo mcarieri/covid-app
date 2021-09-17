74494140a4bfb353f42f0bf3675d40436527b6f5
bowdenshot <anthonyelavezzo@gmail.com>
2021-09-16 23:32:47 -0500

    added event listener at bottom

--- public/js/add-entry.js
+++ public/js/add-entry.js
@@ -1,13 +1,13 @@
 async function newEntryHandler() {
     event.preventDefault();
-  
-    const name = document.querySelector('input[name="post-title"]').value;
-    const zipcode = document.querySelector('input[name="post-title"]').value;
-    const mask = document.querySelector('input[name="post-title"]').value;
-    const distance = document.querySelector('input[name="post-title"]').value;
-    const sanitizer = document.querySelector('input[name="post-title"]').value;
-    const patrons = document.querySelector('input[name="post-title"]').value;
-    const comment = document.querySelector('input[name="post-title"]').value;
+
+    const name = document.querySelector('input[name="entry-name"]').value;
+    const zipcode = document.querySelector('input[name="entry-zipcode"]').value;
+    const mask = document.querySelector('input[name="entry-mask"]').value;
+    const distance = document.querySelector('input[name="entry-distance"]').value;
+    const sanitizer = document.querySelector('input[name="entry-sanitizer"]').value;
+    const patrons = document.querySelector('input[name="entry-patrons"]').value;
+    const comment = document.querySelector('input[name="entry-comment"]').value;
 
     const response = await fetch(`/api/entries`, {
         method: 'POST',
@@ -29,3 +29,6 @@ async function newEntryHandler() {
         alert(response.statusText);
     }
 };
+
+// configure HTML to 
+document.querySelector('.new-entry-form').addEventListener('submit', newEntryHandler)
